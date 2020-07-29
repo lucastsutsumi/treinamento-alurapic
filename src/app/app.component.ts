a313725028189c3f1472d263af7d9a8e7f283d3b
Lucas Tsutsumi Ramos <lucastsutsumiramos@gmail.com>
2020-07-28 22:32:38 -0300

    criacao de novo componente photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PhotoService } from "./photos/photo/photo.service";
+import { Photo } from "./photos/photo/photo";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'alurapic';
-  description = 'Leão';
-  url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg'
+export class AppComponent implements OnInit{
+  photos: Photo[] = [];
+
+  constructor(private photoService: PhotoService) {}
+  
+  ngOnInit():void {
+    this.photoService
+      .listFromUser("flavio")
+      .subscribe(photos => this.photos = photos);
+    
+  }
 }
+
